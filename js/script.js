c4bc288960f53ceb69458b881f377cbeba29dfc4
Will Saunders <wsaunders@knights.ucf.edu>
2016-08-26 23:16:33 -0700

    tweaked the ship motion, added tech glyphs around planets

--- js/script.js
+++ js/script.js
@@ -38,7 +38,6 @@ $(document).ready(function(){
 			}
 		}
 	});
-
 	// ROCKET TRAVEL TO MOUSE
 	var rocket = $('#rocket');
 	var rocketW = rocket.width();
@@ -55,10 +54,11 @@ $(document).ready(function(){
 	var flight =false;
 	var rotate = false;
 	var landing = false;
+	var hasFlown = false;
 
 	$('#message-link').click(function(e){
 		e.preventDefault();
-		$('#contact').children('.info').addClass('form-open');
+		$('#contact').children('.slide-out').children('.info').addClass('form-open');
 		//$('#contact-form').show();
 	});
 
@@ -66,21 +66,21 @@ $(document).ready(function(){
 		$('.form-open').removeClass('form-open');
 	});
 	var i = 0;
-	$('#wrapper').click(function(e){
-		$(this).append('<div class="waypoint id-'+i+'"></div');
-		$('.id-'+i).css({top:e.pageY-5,left:e.pageX-5});
-		if(i!=0){
-			var angle = -Math.degrees(Math.atan2((e.pageX-5)-$('.id-'+(i-1)).position().left,(e.pageY-5)-$('.id-'+(i-1)).position().top));
-			if(angle > 0){ // fixes angles so 0 is up, and positive is on right side to sync with css
-				angle = angle-180;
-			}else {
-				angle = angle+180;
-			}
-			distance = Math.sqrt((Math.pow(((e.pageX-5) - $('.id-'+(i-1)).position().left-5),2)) + (Math.pow(((e.pageY-5) - $('.id-'+(i-1)).position().top-5),2)));
-		$(this).append('<div class="line" style="transform-origin:left center;width:'+distance+'px;top:'+ ($('.id-'+(i-1)).position().top+5)+'px;left:'+ ($('.id-'+(i-1)).position().left+5)+'px;transform:rotate('+(angle-90)+'deg);"></div>');
-		}
-		i++;
-	});
+	// $('#wrapper').click(function(e){ // Line debug / path illustrator.
+	// 	$(this).append('<div class="waypoint id-'+i+'"></div');
+	// 	$('.id-'+i).css({top:e.pageY-5,left:e.pageX-5});
+	// 	if(i!=0){
+	// 		var angle = -Math.degrees(Math.atan2((e.pageX-5)-$('.id-'+(i-1)).position().left,(e.pageY-5)-$('.id-'+(i-1)).position().top));
+	// 		if(angle > 0){ // fixes angles so 0 is up, and positive is on right side to sync with css
+	// 			angle = angle-180;
+	// 		}else {
+	// 			angle = angle+180;
+	// 		}
+	// 		distance = Math.sqrt((Math.pow(((e.pageX-5) - $('.id-'+(i-1)).position().left-5),2)) + (Math.pow(((e.pageY-5) - $('.id-'+(i-1)).position().top-5),2)));
+	// 	$(this).append('<div class="line" style="transform-origin:left center;width:'+distance+'px;top:'+ ($('.id-'+(i-1)).position().top+5)+'px;left:'+ ($('.id-'+(i-1)).position().left+5)+'px;transform:rotate('+(angle-90)+'deg);"></div>');
+	// 	}
+	// 	i++;
+	// });
 	$('#wrapper').mousedown(function(e){
 		 mouseX = e.pageX;
 		 mouseY = e.pageY;
@@ -147,68 +147,142 @@ $(document).ready(function(){
 	}).mouseleave(function(e){
 		//setWaypoint(e.pageX,e.pageY);
 		if(e.button ==0  && !isLanding){
-			$('#wrapper').off('mousemove');
-			window.clearInterval(waypointInterval);
-			waypointInterval = false;
+			//$('#wrapper').off('mousemove');
+			//window.clearInterval(waypointInterval);
+			//waypointInterval = false;
 		}
 	});
+	
+	//Planet Highlight 
+	var swooshTo = false;
+	var swoosh1 = TweenMax.to($('.curve').eq(0),8,{rotation:360,repeat:-1,ease:'linear'});
+	var swoosh2 = TweenMax.to($('.curve').eq(1),8,{rotation:510,repeat:-1,ease:'linear'});
+	var swoosh3 = TweenMax.to($('.curve').eq(2),8,{rotation:-360,repeat:-1,ease:'linear'});
+	$('.planet').on('mouseover', function(e){
+		if($(this).is($('.planet').eq(0))) {
+			swoosh1.pause();
+			degrees=156;
+		}else if($(this).is($('.planet').eq(1))){
+			swoosh2.pause();
+			degrees=334;
+		}else {
+			swoosh3.pause();
+			degrees=157;
+		}
+		swooshTo = new TweenMax.to($(this).siblings('.curve'),.5,{directionalRotation:degrees+'_short',ease:Power3.easeOut, onComplete:function(){
+			$(this.target[0]).next().fadeIn(500);
+		}});
+		
+	}).on('mouseleave', function(e){
+		swooshTo.kill();
+			$(this).siblings('.slide-out').fadeOut(500,function(){
+				//swoosh = TweenMax.to($('.curve'),8,{directionalRotation:360+'_cw',repeat:-1,ease:'linear'});
+				console.log($(this).parent().index());
+				if($(this).parent().index()== 0) {
+						swoosh1.resume(3.4666, true);
+				}else if($(this).parent().index() == 1){
+						swoosh2.resume(4.08888, true);
+				}else {
+						swoosh3.resume(4.5666, true);
+				}
+			});
+			
+	});
 
-	var distance =0; //Math.sqrt((Math.pow((x2 -x1),2)) + (Math.pow((y2-y1),2)))
 
 	function setWaypoint(x,y){
-		if(!isFlying){
-			rocket.children('#rocket-img').toggleClass('glow');
-			isFlying = true;
-		}else {
-
-			//flight.kill();
-			//rotate.kill();
-			//rocketY = Math.round(rocket.position().top);
-		//	rocketX = Math.round(rocket.position().left);
-		}
-		var angle = -Math.degrees(Math.atan2(x-rocketCenter.x, y-rocketCenter.y));
+		var distance = Math.abs(Math.sqrt((Math.pow(x - (rocketX-50),2)) + (Math.pow(y - (rocketY-92.5),2))));
+		var angle = -Math.degrees(Math.atan2(x-(rocketX + 50), y-(rocketY + 92.5)));
 		if(angle > 0){ // fixes angles so 0 is up, and positive is on right side to sync with css
 			angle = angle-180;
 		}else {
 			angle = angle+180;
 		}
 		console.log('Angle: '+angle);
-		rotate = TweenMax.to( rocket, 0.5, {directionalRotation:(angle)+'_short'});
-		flight = TweenMax.to( rocket, 2, {top:y-rocketOrigin*2, left:x - rocketOrigin, onComplete:function(){
+		console.log(distance);
+		rotate = new TweenMax.to( rocket, 0.5, {directionalRotation:(angle)+'_short'});
+		if(!isFlying){
 			rocket.children('#rocket-img').toggleClass('glow');
-			isFlying=false;
-			
-			var tr=$('#rocket').css('transform');
-			var values = tr.split('(')[1];
-			    values = values.split(')')[0];
-			    values = values.split(',');
-			var a = values[0];
-			var b = values[1];
-			var c = values[2];
-			var d = values[3];
-			var cAngle = Math.round(Math.atan2(b, a) * (180/Math.PI));
-			console.log('Final Angle: '+cAngle);
+			isFlying = true;
+			flight = TweenMax.to( rocket, (.5*(distance/125)), {top:y-rocketOrigin*2, left:x - rocketOrigin, ease:Power2.easeInOut,
+			onUpdate:function(){
+				rocketY = Math.round(rocket.position().top);
+				rocketX = Math.round(rocket.position().left);
+			},
+			onComplete:function(){
+				rocket.children('#rocket-img').toggleClass('glow');
+				isFlying=false;
+				
+				var tr=$('#rocket').css('transform');
+				var values = tr.split('(')[1];
+				    values = values.split(')')[0];
+				    values = values.split(',');
+				var a = values[0];
+				var b = values[1];
+				var c = values[2];
+				var d = values[3];
+				var cAngle = Math.atan2(b, a) * (180/Math.PI);
+				console.log('Final Angle: '+cAngle);
 
-			if(checkCollision(x,y)){
-				$('#pane').fadeIn(1000);
-				$('.info').fadeOut(1000);
+				if(checkCollision(x,y)){
+					$('#pane').fadeIn(1000);
+					$('.slide-out').fadeOut(1000);
 
-				isLanding = true;
-				landing = new TweenMax.to(rocket.children('#rocket-img'),2.5,{scale:0, onComplete:function(){
-					rocketY = Math.round(rocket.position().top);
-					rocketX = Math.round(rocket.position().left);
+					isLanding = true;
+					landing = new TweenMax.to(rocket.children('#rocket-img'),2.5,{scale:0, onComplete:function(){
+						rocketY = Math.round(rocket.position().top);
+						rocketX = Math.round(rocket.position().left);
 
-					isLanded = true;	
-					isLanding = false;
-				}});
-			}
-			// GSAP Bezier plugin.
-			// var xDiff = ((x-rocketX)/2);
-			// var cpX = rocketX;
-			// var cpY = y+xDiff;
-			// var fly = new TweenMax.to(rocket, 5, {bezier:{curviness:1.25, values:[{x:100, y:250}, {x:300, y:0}, {x:500, y:400}], autoRotate:true}, ease:Power1.easeInOut});
-		}});
-	}
+						isLanded = true;	
+						isLanding = false;
+					}});
+				}
+			}});
+		}else {
+			isFlying = true;
+			flight.kill();
+			flight = TweenMax.to( rocket, (.5*(distance/125)), {top:y-rocketOrigin*2, left:x - rocketOrigin, ease:Power2.easeOut,
+			onUpdate:function(){
+				rocketY = Math.round(rocket.position().top);
+				rocketX = Math.round(rocket.position().left);
+			},
+			onComplete:function(){
+				rocket.children('#rocket-img').toggleClass('glow');
+				isFlying=false;
+				
+				var tr=$('#rocket').css('transform');
+				var values = tr.split('(')[1];
+				    values = values.split(')')[0];
+				    values = values.split(',');
+				var a = values[0];
+				var b = values[1];
+				var c = values[2];
+				var d = values[3];
+				var cAngle = Math.atan2(b, a) * (180/Math.PI);
+				console.log('Final Angle: '+cAngle);
+
+				if(checkCollision(x,y)){
+					$('#pane').fadeIn(1000);
+					$('.info').fadeOut(1000);
+
+					isLanding = true;
+					landing = new TweenMax.to(rocket.children('#rocket-img'),2.5,{scale:0, onComplete:function(){
+						rocketY = Math.round(rocket.position().top);
+						rocketX = Math.round(rocket.position().left);
+
+						isLanded = true;	
+						isLanding = false;
+					}});
+				}
+				// GSAP Bezier plugin.
+				// var xDiff = ((x-rocketX)/2);
+				// var cpX = rocketX;
+				// var cpY = y+xDiff;
+				// var fly = new TweenMax.to(rocket, 5, {bezier:{curviness:1.25, values:[{x:100, y:250}, {x:300, y:0}, {x:500, y:400}], autoRotate:true}, ease:Power1.easeInOut});
+			}});
+		}
+		
+	}///End Function
 
 	function checkCollision(x,y){
 		var collided = false;
@@ -249,6 +323,9 @@ $(document).ready(function(){
 		writerPlanet = new Planet($('#writer'));
 		$('body').fitText(1.5);
 		$('#intro').css({top:(h-$('#intro').height())/2});
+		if(!hasFlown){
+			$('#rocket').css({left:(w - $('#rocket').width())/2});
+		}
 	});
 
 }); //END READY
