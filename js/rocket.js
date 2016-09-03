c9e6261fc3d13a6543ad7cc18dc3adf957e5765a
Will Saunders <wsaunders@knights.ucf.edu>
2016-09-03 15:22:13 -0700

    curving rocket working! Two known bugs, need to reconfigure pathing to points BEHIND ship. Ship jitters for a frame when clicking new path while flying.

--- js/rocket.js
+++ js/rocket.js
@@ -75,33 +75,16 @@ $('#wrapper').on('click', function(e){
 	//console.log('mouse: '+mouseX+', '+mouseY)
 	var absAngle  = getAngle(mouseX,Rocket.centerX,mouseY,Rocket.centerY);
 	var angleDistance = Math.sqrt((Math.pow((mouseX - (Rocket.left+Rocket.halfX)),2)) + (Math.pow((mouseY-(Rocket.top+Rocket.halfY)),2)));
-	console.log('abs Distance: '+angleDistance);
-	console.log('Angle: '+absAngle);
-
+	//console.log('abs Distance: '+angleDistance);
+	//console.log('Angle: '+absAngle);
 	var cp2Angle = (-180- Rocket.angle) +(absAngle*2);
 	if(cp2Angle < -360)
 		cp2Angle+=360;
 	if(cp2Angle > 360)
 		cp2Angle -=360;
 
-	console.log('cp2Angle: '+cp2Angle);
-	/*
-		   0 =  0.5    0/180 - 0.5 =  -0.5
-		 -45 = 0.25   45/180 - 0.5 = -0.25
-		 -90 =    0   90/180 - 0.5 =     0
-		-135 = 0.25  135/180 - 0.5 =  0.25
-		-180 =  0.5  180/180 - 0.5 =   0.5
-		 135 = 0.25  135/180 - 0.5 =  0.25
-		  90 =    0   90/180 - 0.5 =     0
-		  45 = 0.25   45/180 - 0.5 =  0.25
-		   0 =  0.5    0/180 - 0.5 =   0.5
-	*/
-	var distRatio = Math.abs(absAngle/180) - 0.5;
-	// 99 /90/2
-	console.log('DistRatio: '+distRatio);
+	var distRatio = Math.abs((Math.abs(Rocket.angle) - Math.abs(absAngle))/180);
 	var cp2Distance = angleDistance* distRatio;
-	//var cp2Distance = angleDistance*.5;
-	//console.log('cp2Dist: '+cp2Distance);
 	var cp1X,cp1Y,cp2X,cp2Y;
 	cp1X = Rocket.centerX + cp2Distance* (Math.cos(Math.radians(Rocket.angle)));
 	cp1Y = Rocket.centerY + cp2Distance* (Math.sin(Math.radians(Rocket.angle)));
@@ -114,13 +97,13 @@ $('#wrapper').on('click', function(e){
 
 	
 	//Red Line
-	$(this).append('<div class="line" style="transform-origin:left center;width:'+(Math.round(angleDistance))+'px;top:'+(Rocket.centerY)+'px;left:'+(Rocket.centerX)+'px;transform:rotate('+(Math.round(absAngle))+'deg);"></div>');
-	//Blue Line
-	$(this).append('<div class="line" style="background:#0000FF;transform-origin:left center;width:'+Math.round(cp2Distance)+'px;top:'+(mouseY)+'px;left:'+(mouseX)+'px;transform:rotate('+(Math.round(cp2Angle))+'deg);"></div>');
-	$(this).append('<div class="line" style="background:#0000FF;transform-origin:left center;width:'+(+cp2Distance)+'px;top:'+(Rocket.centerY)+'px;left:'+(Rocket.centerX)+'px;transform:rotate('+(Rocket.angle)+'deg);"></div>');
+	// $(this).append('<div class="line" style="transform-origin:left center;width:'+(Math.round(angleDistance))+'px;top:'+(Rocket.centerY)+'px;left:'+(Rocket.centerX)+'px;transform:rotate('+(Math.round(absAngle))+'deg);"></div>');
+	// //Blue Line
+	// $(this).append('<div class="line" style="background:#0000FF;transform-origin:left center;width:'+Math.round(cp2Distance)+'px;top:'+(mouseY)+'px;left:'+(mouseX)+'px;transform:rotate('+(Math.round(cp2Angle))+'deg);"></div>');
+	// $(this).append('<div class="line" style="background:#0000FF;transform-origin:left center;width:'+(+cp2Distance)+'px;top:'+(Rocket.centerY)+'px;left:'+(Rocket.centerX)+'px;transform:rotate('+(Rocket.angle)+'deg);"></div>');
 	
 	//console.log(curve.getLUT());
-	$('body').append('<div class="waypoint" style="background:#ff0000;top:'+(cp1Y-5) +'px;left:'+(cp1X-5)+'px;"></div>');
+	// $('body').append('<div class="waypoint" style="background:#ff0000;top:'+(cp1Y-5) +'px;left:'+(cp1X-5)+'px;"></div>');
 	//$('body').append('<div class="waypoint" style="background:#ff0000;top:'+(polarY-5) +'px;left:'+(polarX-5)+'px;"></div>');
 	for(i=0;i<curveP.length;i+=4){
 		$('body').append('<div class="waypoint" style="top:'+(curveP[i].y-5) +'px;left:'+(curveP[i].x-5)+'px;"></div>');
@@ -134,6 +117,7 @@ $('#wrapper').on('click', function(e){
 		var t = progress/time;
 		var coords = curve.get(t);
 		var nextAngle = getAngle(coords.x,oldCoords.x,coords.y,oldCoords.y);
+		Rocket.obj.addClass('moving');
 		//console.log(coords);
 		Rocket.angle = nextAngle;
 		Rocket.obj.css({left: coords.x-Rocket.halfY,top:coords.y-Rocket.halfX,transform:'rotate('+nextAngle+'deg)'});
@@ -142,8 +126,9 @@ $('#wrapper').on('click', function(e){
 		if(progress < time)
 			window.requestAnimationFrame(move);
 		else {
+			Rocket.obj.removeClass('moving');
 			//console.log('Final Angle: '+Rocket.angle);
-			console.log(' ');
+			//console.log(' ');
 		}
 
 	}
